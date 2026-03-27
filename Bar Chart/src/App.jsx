fec091accffa326e6065dfbf8717e5a7499b0b15
Abdul Haseeb <abdulhaseebah7886786@gmail.com>
2026-03-27 16:10:48 +0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   Bar Chart/src/App.jsx

--- Bar Chart/src/App.jsx
+++ Bar Chart/src/App.jsx
@@ -1,16 +1,7 @@
 "use client";
 
 import {
-  LineChart, Line,
   BarChart, Bar,
-  AreaChart, Area,
-  PieChart, Pie, Cell,
-  RadarChart, Radar, PolarGrid, PolarAngleAxis, PolarRadiusAxis,
-  RadialBarChart, RadialBar,
-  ScatterChart, Scatter,
-  ComposedChart,
-  Treemap,
-  FunnelChart, Funnel,
   XAxis, YAxis, CartesianGrid, Tooltip, Legend,
   ResponsiveContainer
 } from "recharts";
@@ -28,19 +19,6 @@ const data = [
 export default function App() {
   return (
     <div>
-      {/* <h1>Line Charts</h1>
-      <ResponsiveContainer width="100%" height={300}>
-        <LineChart data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-        </LineChart>
-      </ResponsiveContainer> */}
-
       <h1>Bar Charts</h1>
       <ResponsiveContainer width="100%" height={300}>
         <BarChart data={data}>
@@ -53,89 +31,6 @@ export default function App() {
           <Bar dataKey="uv" fill="#82ca9d" />
         </BarChart>
       </ResponsiveContainer>
-
-      <h1>Area Chart</h1>
-        <ResponsiveContainer width="100%" height={300}>
-          <AreaChart data={data}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Area type="monotone" dataKey="pv" stroke="#8884d8" fill="#8884d8" />
-          </AreaChart>
-        </ResponsiveContainer>
-
-      <h1>Pie Chart</h1>
-        <ResponsiveContainer width="100%" height={300}>
-          <PieChart>
-            <Pie data={data} dataKey="pv" nameKey="name" outerRadius={100}>
-              {data.map((_, i) => (
-                <Cell key={i} fill={`hsl(${i * 60}, 70%, 50%)`} />
-              ))}
-            </Pie>
-            <Tooltip />
-          </PieChart>
-        </ResponsiveContainer>
-
-      <h1>Composed Chart</h1>
-        <ResponsiveContainer width="100%" height={300}>
-          <ComposedChart data={data}>
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Bar dataKey="uv" fill="#82ca9d" />
-            <Line dataKey="pv" stroke="#8884d8" />
-          </ComposedChart>
-        </ResponsiveContainer>
-
-      <h1>Scatter Chart</h1>
-        <ResponsiveContainer width="100%" height={300}>
-          <ScatterChart>
-            <CartesianGrid />
-            <XAxis dataKey="uv" name="UV" />
-            <YAxis dataKey="pv" name="PV" />
-            <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-            <Scatter data={data} fill="#8884d8" />
-          </ScatterChart>
-        </ResponsiveContainer>
-
-      <h1>Radar Chart</h1>
-        <ResponsiveContainer width="100%" height={300}>
-          <RadarChart data={data}>
-            <PolarGrid />
-            <PolarAngleAxis dataKey="name" />
-            <PolarRadiusAxis />
-            <Radar dataKey="pv" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-          </RadarChart>
-        </ResponsiveContainer>
-
-      <h1>Radial Bar Chart</h1>
-        <ResponsiveContainer width="100%" height={300}>
-          <RadialBarChart innerRadius="20%" outerRadius="90%" data={data}>
-            <RadialBar dataKey="pv" fill="#8884d8" />
-            <Tooltip />
-          </RadialBarChart>
-        </ResponsiveContainer>
-
-      <h1>TreeMap Chart</h1>
-        <ResponsiveContainer width="100%" height={300}>
-          <Treemap
-            data={data}
-            dataKey="pv"
-            stroke="#fff"
-            fill="#8884d8"
-          />
-        </ResponsiveContainer>
-
-      <h1>Funnel Chart</h1>
-        <ResponsiveContainer width="100%" height={300}>
-          <FunnelChart>
-            <Tooltip />
-            <Funnel dataKey="pv" data={data} isAnimationActive />
-          </FunnelChart>
-        </ResponsiveContainer>
-      
-      
     </div>
   );
 }
